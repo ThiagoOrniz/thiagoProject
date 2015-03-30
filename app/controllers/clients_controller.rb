587a142310a85e5db1c6e89aae9c1a9d2d621df6
ThiagoOrniz <thiago.orniz@gmail.com>
2015-03-30 22:07:57 +0000

    add view client add edit/destroy

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,51 @@
 class ClientsController < ApplicationController
-
+	before_action :find_client, only: [:show, :edit, :update, :destroy]
+	
 	def index
+	end
+
+	def show
 		
+	end
+
+	def create
+		@client = Client.new(client_params)
+
+		if @client.save
+			redirect_to @client
+		else
+			render 'new'
+		end
+	end
+
+	def new
+		@client = Client.new
+	end
+
+	def edit
+	end
+
+	def update
+		if @client.update(client_params)
+			redirect_to @client
+
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@client.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def client_params
+		params.require(:client).permit(:name, :address, :phone, :email)
+	end
+
+	def find_client
+		@client = Client.find(params[:id])
 	end
 end
